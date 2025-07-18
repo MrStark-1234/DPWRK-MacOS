188b527f673a3ce3834fcbd02f7a76ee0ff06f90
Johan Biju paul <108187150+MrStark-1234@users.noreply.github.com>
2025-07-18 11:59:53 +0530

    Initial Commit

---
+++ DPWRK/DPWRKApp.swift
@@ -0,0 +1,17 @@
+//
+//  DPWRKApp.swift
+//  DPWRK
+//
+//  Created by Johan Biju Paul on 18/07/25.
+//
+
+import SwiftUI
+
+@main
+struct DPWRKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DPWRKTests/DPWRKTests.swift
@@ -0,0 +1,16 @@
+//
+//  DPWRKTests.swift
+//  DPWRKTests
+//
+//  Created by Johan Biju Paul on 18/07/25.
+//
+
+import Testing
+
+struct DPWRKTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
